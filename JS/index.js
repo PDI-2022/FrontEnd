08090eaec7d307bf1500fe85fbb98486814d7cbb
unknown <victorehrich@gmail.com>
2022-09-14 22:05:05 -0300

    reader corrections

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,7 @@
 window.onload = function(){
     localStorage.clear()
 }
-async function dropHandler(event, input){
+function dropHandler(event, input){
     try{
         event.preventDefault();
         if(input != "interna" && input != "externo"){
@@ -9,19 +9,17 @@ async function dropHandler(event, input){
         }
         const item = event.dataTransfer.items
         if(item.length > 0){
-            debugger
             const itemAsFile = item[0].getAsFile()
             const name = itemAsFile.name
 
             var reader = new FileReader();
-            reader.onloadend = await function () {
-                var json =                     {
+            reader.onloadend = function () {
+                var json = {
                     "fileName": `${name}`,
-                    "base64": `${reader.result}` 
-                }
-                localStorage.setItem(input,JSON.stringify(json))
+                    "base64": `${reader.result}`
+                };
+                localStorage.setItem(input, JSON.stringify(json));
             }
-            console.log(localStorage.getItem(input))
             reader.readAsDataURL(itemAsFile);
         }
         else{
