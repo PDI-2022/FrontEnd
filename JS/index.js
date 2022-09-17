2476897c3a08b6f54b5d521e7ad1dab1c86cd81a
Breno <112519@pmenos.com.br>
2022-09-17 13:10:05 -0300

    add modal

--- JS/index.js
+++ JS/index.js
@@ -1,14 +1,14 @@
-window.onload = function(){
+window.onload = function () {
     localStorage.clear()
 }
-function dropHandler(event, input){
-    try{
+function dropHandler(event, input) {
+    try {
         event.preventDefault();
-        if(input != "interna" && input != "externo"){
+        if (input != "interna" && input != "externo") {
             throw new Error("Problema com o tipo do input informado")
         }
         const item = event.dataTransfer.items
-        if(item.length > 0){
+        if (item.length > 0) {
             const itemAsFile = item[0].getAsFile()
             const name = itemAsFile.name
 
@@ -22,15 +22,32 @@ function dropHandler(event, input){
             }
             reader.readAsDataURL(itemAsFile);
         }
-        else{
+        else {
             throw new Error("Arquivo com problema")
         }
     }
-    catch(err){
+    catch (err) {
         console.error(err)
     }
-     
+
 }
-function dragOverHandler(event, input){
+function dragOverHandler(event, input) {
     event.preventDefault();
+}
+var uri = "https://api.adviceslip.com/advice";
+function get() {
+    var req = new XMLHttpRequest();
+    req.addEventListener("readystatechange", function () {
+        if (this.readyState !== 4) {
+            new Promise(() => {
+                $('#modal-comp').modal('show');
+            });
+        }
+        if (this.readyState === 4) {
+            console.log(req.response);
+            $('#modal-comp').modal('hide');
+        }
+    });
+    req.open('GET', uri, false);
+    req.send('');
 }
